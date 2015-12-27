a5de4807eb38ecf113a2f25aa47cf0b22d6832e9
Javier Delgado <dn.javier@hotmail.com>
2015-12-27 17:47:40 -0600

    Paginas en html casas principal, boleta de inicio y cuestionario de filtro.

---
+++ html-prueba/js/init.js
@@ -0,0 +1,8 @@
+(function($) {
+    $(function() {
+
+        $(".dropdown-button").dropdown();
+        $(".button-collapse").sideNav();
+
+    }); // End Document Ready
+})(jQuery); // End of jQuery name space
